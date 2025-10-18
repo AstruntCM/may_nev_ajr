c087a6cc4eb059285bf229b7f914129e7454fcca
Marton Balint Mayer <mayer.marton.balint@gmail.com>
2025-10-18 12:04:04 +0200

    Update CMake and launch files with latest configuration

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -5,30 +5,21 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='turtlesim',
-            namespace='turtlesim1',
             executable='turtlesim_node',
             name='sim'
         ),
         Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
+            package='may_nev_ajr',
+            executable='trafficLight',
+            name='trafficLight',
+            parameters=[{
+                "redLightTime": 4.0,
+                "greenLightTime": 8.0
+            }]
         ),
         Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
-            remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        ),
-        # Node(
-        #     package='may_nev_ajr',
-        #     executable='simple_sub_node',
-        #     output='screen',
-        # ),
-
-        # first comment
+            package='may_nev_ajr',
+            executable='driverNode',
+            name='driverNode',
+        )
     ])
